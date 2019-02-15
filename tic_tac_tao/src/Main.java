dbcbae29622be9bad8082539031e004b5f2117aa
fero656565 <47314609+fero656565@users.noreply.github.com>
2019-02-15 13:02:54 +0100

    tic_tac_toe_fertig

--- tic_tac_tao/src/Main.java
+++ tic_tac_tao/src/Main.java
@@ -5,6 +5,63 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     static int[][] feld = new int[3][3];
     static boolean freiesFeld = true;
+    static int aktSpieler = 1;
+    static boolean nichtGewonnen = true;
+
+
+    static void spielerWechseln() {
+        aktSpieler = (aktSpieler == 1) ? -1 : 1;       // Ternärer Operator
+        // Bedingung ? Was, wenn Bedingung zutrifft : Was, wenn Bedingung nicht zutrifft
+        // Kurzschreibweise für folgendes:
+
+//        if(aktSpieler == 1) {
+//            aktSpieler = -1;
+//        }
+//        else {
+//            aktSpieler = 1;
+//        }
+
+    }
+
+
+    static void gewinnPruefen() {
+
+
+        if (    // Spalten
+                (feld[0][0] == 1 && feld[0][1] == 1 && feld[0][2] == 1) ||
+                (feld[1][0] == 1 && feld[1][1] == 1 && feld[1][2] == 1) ||
+                (feld[2][0] == 1 && feld[2][1] == 1 && feld[2][2] == 1) ||
+                // Reihen
+                (feld[0][0] == 1 && feld[1][0] == 1 && feld[2][0] == 1) ||
+                (feld[0][1] == 1 && feld[1][1] == 1 && feld[1][2] == 1) ||
+                (feld[0][2] == 1 && feld[1][2] == 1 && feld[2][2] == 1) ||
+                // Diagonalen
+                (feld[0][0] == 1 && feld[1][1] == 1 && feld[2][2] == 1) ||
+                (feld[0][2] == 1 && feld[1][1] == 1 && feld[2][0] == 1)) {
+
+            System.out.println("Sieg!! Spieler 1 hat drei in einer Reihe.");
+            nichtGewonnen = false;
+
+
+        } else if ((
+                 // Spalten
+                 feld[0][0] == -1 && feld[0][1] == -1 && feld[0][2] == -1) ||
+                (feld[1][0] == -1 && feld[1][1] == -1 && feld[1][2] == -1) ||
+                (feld[2][0] == -1 && feld[2][1] == -1 && feld[2][2] == -1) ||
+                // Reihen
+                (feld[0][0] == -1 && feld[1][0] == -1 && feld[2][0] == -1) ||
+                (feld[0][1] == -1 && feld[1][1] == -1 && feld[1][2] == -1) ||
+                (feld[0][2] == -1 && feld[1][2] == -1 && feld[2][2] == -1) ||
+                // Diagonalen
+                (feld[0][0] == -1 && feld[1][1] == -1 && feld[2][2] == -1) ||
+                (feld[0][2] == -1 && feld[1][1] == -1 && feld[2][0] == -1)) {
+            nichtGewonnen = false;
+            System.out.println("Sieg!! Spieler 2 hat drei in einer Reihe.");
+
+
+        }
+
+    }
 
     static void Spielsteinsetzen(int x, int y, int wert) {
         feld[x][y] = wert;
@@ -58,7 +115,8 @@ public class Main {
 
         initFeld();
 
-        while (freiesFeld) {
+
+        while (freiesFeld && nichtGewonnen) {
             System.out.println("bitte geben Sie den x + y an");
             System.out.println("");
 
@@ -66,10 +124,15 @@ public class Main {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            Spielsteinsetzen(x, y, -1);
+
+            Spielsteinsetzen(x, y, aktSpieler);
+
+            spielerWechseln();
             pruefeFeld();
             gebeSpielfeld();
+            gewinnPruefen();
         }
+
     }
 }
 

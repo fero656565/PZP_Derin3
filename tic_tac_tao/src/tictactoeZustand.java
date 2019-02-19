389fed2eee040cc019c3f37c34c00e0e83ea33ec
fero656565 <47314609+fero656565@users.noreply.github.com>
2019-02-19 12:47:19 +0100

    tictactoe_finish

--- tic_tac_tao/src/tictactoeZustand.java
+++ tic_tac_tao/src/tictactoeZustand.java
@@ -1,18 +1,36 @@
+
+
 import java.util.Scanner;
 
-public class Main {
+
+public class tictactoeZustand {
 
     static Scanner sc = new Scanner(System.in);
     static int[][] feld = new int[3][3];
     static boolean freiesFeld = true;
     static int aktSpieler = 1;
-    static boolean nichtGewonnen = true;
+    static boolean nichtGewonnen1 = true;
+    static boolean nichtGewonnen2 = true;
+    static String nameSpieler1;
+    static String nameSpieler2;
+    static int Match = 1;
+    static int punkteSpieler1 = 0;
+    static int punkteSpieler2 = 0;
+    static int punkteUnentschieden = 0;
+    static int wiederholeN;
+    static int zustand = 1;
+    static int x;
+    static int y;
 
 
     static void spielerWechseln() {
-        aktSpieler = (aktSpieler == 1) ? -1 : 1;       // Ternärer Operator
-        // Bedingung ? Was, wenn Bedingung zutrifft : Was, wenn Bedingung nicht zutrifft
-        // Kurzschreibweise für folgendes:
+        aktSpieler = ((aktSpieler == 1) ? -1 : 1);       // Ternärer Operator
+
+
+
+
+            // Bedingung ? Was, wenn Bedingung zutrifft : Was, wenn Bedingung nicht zutrifft
+            // Kurzschreibweise für folgendes:
 
 //        if(aktSpieler == 1) {
 //            aktSpieler = -1;
@@ -20,7 +38,18 @@ public class Main {
 //        else {
 //            aktSpieler = 1;
 //        }
+        }
 
+
+    static void begrueßung() {
+        System.out.println("Willkommen beim TicTacToe V1.0");
+    }
+
+    static void spielerName() {
+        System.out.println("Bitte geben Sie den Namen vom Spieler 1 an: ");
+        nameSpieler1 = sc.nextLine();
+        System.out.println("Bitte geben Sie den Namen vom Spieler 2 an: ");
+        nameSpieler2 = sc.nextLine();
     }
 
 
@@ -39,8 +68,9 @@ public class Main {
                         (feld[0][0] == 1 && feld[1][1] == 1 && feld[2][2] == 1) ||
                         (feld[0][2] == 1 && feld[1][1] == 1 && feld[2][0] == 1)) {
 
-            System.out.println("Sieg!! Spieler 1 hat drei in einer Reihe.");
-            nichtGewonnen = false;
+            punkteSpieler1++;
+            System.out.println("Sieg!! " + nameSpieler1 + " hat drei in einer Reihe.");
+            nichtGewonnen1 = false;
 
 
         } else if (
@@ -55,13 +85,24 @@ public class Main {
                         // Diagonalen
                         (feld[0][0] == -1 && feld[1][1] == -1 && feld[2][2] == -1) ||
                         (feld[0][2] == -1 && feld[1][1] == -1 && feld[2][0] == -1)) {
-            nichtGewonnen = false;
-            System.out.println("Sieg!! Spieler 2 hat drei in einer Reihe.");
+            nichtGewonnen2 = false;
+            punkteSpieler2++;
+            System.out.println("Sieg!! " + nameSpieler2 + " hat drei in einer Reihe.");
+
         }
     }
 
-    static void Spielsteinsetzen (int x, int y, int wert){
+    static void Spielsteinsetzen(int x, int y, int wert) {
+
+        do {
+            gebeSpielfeld();
+            System.out.println("X: ");
+            x = sc.nextInt();
+            System.out.println("Y: ");
+            y = sc.nextInt();
+        } while(feld[x][y] != 0);
         feld[x][y] = wert;
+
     }
 
 
@@ -86,7 +127,7 @@ public class Main {
         }
     }
 
-    static void initFeld () {
+    static void initFeld() {
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
                 feld[x][y] = 0;
@@ -94,9 +135,23 @@ public class Main {
         }
     }
 
-    static void pruefeFeld () {
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
+
+    static void setWiederholeN() {
+
+        System.out.println("Möchtet ihr eine neue Runde Starten? '1' für neue Runde starten eingeben");
+        wiederholeN = sc.nextInt();
+    }
+
+
+    static void punkteZähler() {
+        System.out.println("Runde: " + Match + " . " + nameSpieler1 + " hat " + punkteSpieler1 + " Punkte und " + nameSpieler2 +
+                " hat " + punkteSpieler2 + " Punkte");
+
+    }
+
+    static void pruefeFeld(int x, int y) {
+        for (y = 0; y < 3; y++) {
+            for (x = 0; x < 3; x++) {
                 if (feld[x][y] == 0) {
                     // Freies Feld gefunden
                     return;
@@ -105,34 +160,47 @@ public class Main {
         }
 
         // Kein freies Feld gefunden
+        punkteUnentschieden++;
         freiesFeld = false;
+        System.out.println("Es sind keine freie Felder mehr Vorhanden. Es steht unentschieden!");
 
     }
 
-    public static void main (String[]args){
+    public static void main(String[] args) {
 
+        wiederholeN = 1;
+        begrueßung();
+
+        spielerName();
         initFeld();
 
-
-        while (freiesFeld && nichtGewonnen) {
-            System.out.println("Spieler " + ((aktSpieler == 1) ? "1" : "2") + " Sie sind am Zug, viel Glück :)");
-            System.out.println("");
+        while (wiederholeN == 1) {
+            while (freiesFeld && nichtGewonnen1 && nichtGewonnen2) {
 
 
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+                System.out.println(((aktSpieler == 1) ? nameSpieler1 : nameSpieler2) + " Sie sind am Zug, viel Glück");
+                System.out.println("");
 
 
-            Spielsteinsetzen(x, y, aktSpieler);
+                Spielsteinsetzen(x, y, aktSpieler);
 
-            spielerWechseln();
-            pruefeFeld();
-            gebeSpielfeld();
-            gewinnPruefen();
+                spielerWechseln();
+                pruefeFeld(x, y);
+               // gebeSpielfeld();
+                gewinnPruefen();
+                punkteZähler();
+
+//while schleife verschachteln dass setwiederhole erst am schluss durchgeführt wird;
+            }
+            setWiederholeN();
+
         }
 
     }
 }
+
+
+
 
 
 
